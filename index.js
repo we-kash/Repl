66718b8dc07e17517544b360a7deb1dbc508d768
VikashSaxena <21170154-VikashSaxena@users.noreply.replit.com>
2024-01-21 19:14:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// // ECMA script
+// Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. runtime and not a language, a runtime is a program that is used to run code outside of a browser. and not a language, a language is a set of rules that are used to communicate with a computer.
+
+// // Node.js is an event-driven, non-blocking, asynchronous I/O runtime that uses
+
+const http = require("http");
+const express = require("express");
+const app = express();
+
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
